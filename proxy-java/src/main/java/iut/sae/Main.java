9b745ba52f7e89c23c6fc97cc6759cfa73ab9b81
Nicolas Claude <nicolas.claude7@etu.univ-lorraine.fr>
2026-06-02 11:39:10 +0200

    add : healthcheck simple

---
+++ proxy-java/src/main/java/iut/sae/Main.java
@@ -0,0 +1,26 @@
+package iut.sae;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        server.createContext("/api/health", exchange -> {
+            String response = "{\"status\":\"ok\"}";
+
+            exchange.getResponseHeaders().add("Content-Type", "application/json");
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        });
+
+        server.start();
+
+        System.out.println("Proxy lancé sur http://localhost:8080");
+    }
+}
